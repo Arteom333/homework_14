833e6566485ffb3a073dccba4404e517b11a8a32
Arteom333 <porchiesku@bk.ru>
2018-11-22 15:33:01 +0200

    APT

---
+++ script14.js
@@ -0,0 +1,26 @@
+var a = 2;
+var b = 3;
+var c = -9;
+
+function calculateDiscriminant(a, b, c, discrim) {
+    
+    return discrim = b * b - 4 * a * c;
+}
+function calculateRoots1(x, x1, x2) {
+    if (discrim < 0) {
+        return x = " Нет действительных корней";
+    } else if (discrim = 0) {
+        return x = -b / 2 * a;
+        
+     } else (discrim > 0); {
+         return x1,x2 = -b +- Math.sqrt(discrim) / 2 * a;
+        
+     }
+}
+
+// && x2 = -b - Math.sqrt(discrim) / 2 * a;
+
+
+
+    
+    
